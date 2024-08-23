909129490af55842c49478cc3af114dcf4895669
rmoran21 <rmoran2001@gmail.com>
2024-08-23 15:38:54 -0600

    actualizacion texto

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express(); // Corrección de 'expres' a 'express'
 
-app.get('/', (req, res) => res.send('Hola mundo con expres')); // Corrección de paréntesis adicional y formato
+app.get('/', (req, res) => res.send('<h1>Hola mundo con expres y HTML</h1>')); // Corrección de paréntesis adicional y formato
 
 app.listen(3000, () => {
   console.log('Server on port 3000'); // Mover el console.log dentro del callback para confirmar que el servidor se ha iniciado correctamente
